4abd5a1e8aefad586977f6a3c8c30a6c77e0f712
Dmitry-Poltaradnev <poltaradnev@gmail.com>
2023-07-11 18:19:32 +0400

    added pagination

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,11 +4,15 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock.jsx";
 import Skeleton from "../components/PizzaBlock/Skeleton.jsx";
+import Pagination from '../components/Pagination/index'
 
  const Home = ({searchValue}) => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setcategoryId] = React.useState(0)
+
+  const [currentPage, setCurrentPage] = React.useState(1)
+
   const [sortType,setSortType] = React.useState({name:'популярности', sortProperty:'rating'}) 
 
   React.useEffect(() => {
@@ -17,8 +21,9 @@ import Skeleton from "../components/PizzaBlock/Skeleton.jsx";
     const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
     const sortBy = sortType.sortProperty.replace('-','')
     const category = categoryId > 0 ? `category=${categoryId}` : ''
+    const search = searchValue ? `&search=${searchValue}` : ''
 
-    fetch(`https://645a6a4a95624ceb210148ff.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`)
+    fetch(`https://645a6a4a95624ceb210148ff.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
       .then((res) => {
         return res.json();
       })
@@ -27,14 +32,21 @@ import Skeleton from "../components/PizzaBlock/Skeleton.jsx";
         setIsLoading(false);
       });
       window.scrollTo(0,0)
-  }, [categoryId,sortType]);
+  }, [categoryId,sortType,searchValue, currentPage]);
 
-  const pizzas = items.filter(obj => {
-    if(obj.title.includes(searchValue)){
+  // Фильтрация при динамике
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
+
+  // Фильтрация товаров при статике.
+ /*  const pizzas = items.filter(obj => {
+    if(obj.title.toLowerCase().includes(searchValue.toLowerCase())){
       return true
     }
     return false
-  }).map((obj) => <PizzaBlock key={obj.id} {...obj} />)
+  }).map((obj) => <PizzaBlock key={obj.id} {...obj} />) */
+
+
+
   const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />)
 
   return (    
@@ -49,6 +61,7 @@ import Skeleton from "../components/PizzaBlock/Skeleton.jsx";
                 ? skeletons
                 : pizzas}
             </div>
+           <Pagination onChangePage={(number) => setCurrentPage(number)}/>
       </div>
     
   )
